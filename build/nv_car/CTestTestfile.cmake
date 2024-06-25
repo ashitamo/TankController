757c70cf7033040a74c64aac1c5b00248d928ebf
Bacon-Su <nick92816@gmail.com>
2024-06-25 11:31:38 +0800

    update

--- build/nv_car/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/baconsu/project/TankController/src/nv_car
-# Build directory: /home/baconsu/project/TankController/build/nv_car
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
