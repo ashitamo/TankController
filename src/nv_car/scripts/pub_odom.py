ad9e1de31af5ca474a0acec13784c610b39c5570
Bacon-Su <nick92816@gmail.com>
2024-07-30 16:49:17 +0800

    imu to odom update

--- src/nv_car/scripts/pub_odom.py
+++ src/nv_car/scripts/pub_odom.py
@@ -1,82 +1,91 @@
+#!/usr/bin/env python
+
 import rospy
-from nav_msgs.msg import Odometry
-from std_msgs.msg import Header
-from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3, TransformStamped, Transform
+import tf
 from sensor_msgs.msg import Imu
-from tf.transformations import euler_from_quaternion, quaternion_from_euler, quaternion_matrix
-import tf2_ros
+from nav_msgs.msg import Odometry
+from math import sin, cos
 
-
-class ImuToOdomConverter:
+class ImuOdom:
     def __init__(self):
-        rospy.init_node('imu_to_odom', anonymous=True)
+        rospy.init_node('imu_odom_node')
         
-        self.sub_imu = rospy.Subscriber('/imu/data', Imu, self.imu_callback)
-        self.pub_odom = rospy.Publisher('/odom', Odometry, queue_size=10)
-        
+        self.odom_pub = rospy.Publisher('/odom', Odometry, queue_size=10)
+        self.imu_sub = rospy.Subscriber('/imu/data', Imu, self.imu_callback)
+
+        self.br = tf.TransformBroadcaster()
         self.odom_msg = Odometry()
-        self.last_imu_data = None
-        self.last_update_time = rospy.Time.now()
+        self.odom_msg.header.frame_id = "odom"
+        self.odom_msg.child_frame_id = "base_link"
+        
+        self.last_time = rospy.Time.now()
+        self.x = 0.0
+        self.y = 0.0
+        self.th = 0.0
+        self.vx = 0.0
+        self.vy = 0.0
+        self.vth = 0.0
 
-        self.tf_broadcaster = tf2_ros.TransformBroadcaster()
-    
-    def imu_callback(self, imu_msg):
-        if self.last_imu_data is None:
-            self.last_imu_data = imu_msg
-            self.last_update_time = imu_msg.header.stamp
-            return
+        # For low-pass filter
+        self.alpha = 0.1
+        self.last_angular_velocity_z = 0.0
 
-        self.odom_msg.header.frame_id = "map"
-        self.odom_msg.child_frame_id = "odom"
+    def imu_callback(self, data):
+        current_time = rospy.Time.now()
+        dt = (current_time - self.last_time).to_sec()
 
-        current_time = imu_msg.header.stamp
-        dt = (current_time - self.last_update_time).to_sec()
+        # Apply low-pass filter to angular velocity
+        self.vth = self.alpha * data.angular_velocity.z + (1 - self.alpha) * self.last_angular_velocity_z
+        self.last_angular_velocity_z = self.vth
 
-        q = (imu_msg.orientation.x, imu_msg.orientation.y, imu_msg.orientation.z, imu_msg.orientation.w)
-        roll, pitch, yaw = euler_from_quaternion(q)
+        # Zero-Velocity Update (ZUPT)
+        if abs(data.angular_velocity.z) < 0.01 and abs(data.linear_acceleration.x) < 0.01 and abs(data.linear_acceleration.y) < 0.01:
+            self.vx = 0.0
+            self.vy = 0.0
+            self.vth = 0.0
 
-        angular_vel_x = imu_msg.angular_velocity.x
-        angular_vel_y = imu_msg.angular_velocity.y
-        angular_vel_z = imu_msg.angular_velocity.z
+        delta_th = self.vth * dt
 
-        linear_accel_x = imu_msg.linear_acceleration.x
-        linear_accel_y = imu_msg.linear_acceleration.y
-        linear_accel_z = imu_msg.linear_acceleration.z
+        # Integrate acceleration to get velocity
+        self.vx += data.linear_acceleration.x * dt
+        self.vy += data.linear_acceleration.y * dt
 
+        delta_x = self.vx * cos(self.th) * dt - self.vy * sin(self.th) * dt
+        delta_y = self.vx * sin(self.th) * dt + self.vy * cos(self.th) * dt
+
+        self.x += delta_x
+        self.y += delta_y
+        self.th += delta_th
+
+        # Update orientation
+        orientation_quat = tf.transformations.quaternion_from_euler(0, 0, self.th)
+        
         self.odom_msg.header.stamp = current_time
-        self.odom_msg.header.frame_id = "map"
-        self.odom_msg.child_frame_id = "odom"
+        self.odom_msg.pose.pose.position.x = self.x
+        self.odom_msg.pose.pose.position.y = self.y
+        self.odom_msg.pose.pose.position.z = 0.0
+        self.odom_msg.pose.pose.orientation.x = orientation_quat[0]
+        self.odom_msg.pose.pose.orientation.y = orientation_quat[1]
+        self.odom_msg.pose.pose.orientation.z = orientation_quat[2]
+        self.odom_msg.pose.pose.orientation.w = orientation_quat[3]
 
-        self.odom_msg.twist.twist.linear.x += dt * linear_accel_x
-        self.odom_msg.twist.twist.linear.y += dt * linear_accel_y
-        self.odom_msg.twist.twist.linear.z += dt * linear_accel_z
+        self.odom_msg.twist.twist.linear.x = self.vx
+        self.odom_msg.twist.twist.linear.y = self.vy
+        self.odom_msg.twist.twist.angular.z = self.vth
 
-        self.odom_msg.pose.pose.position.x += dt * self.odom_msg.twist.twist.linear.x
-        self.odom_msg.pose.pose.position.y += dt * self.odom_msg.twist.twist.linear.y
-        self.odom_msg.pose.pose.position.z += dt * self.odom_msg.twist.twist.linear.z
+        self.odom_pub.publish(self.odom_msg)
 
-        self.odom_msg.pose.pose.orientation = imu_msg.orientation
+        self.br.sendTransform((self.x, self.y, 0), 
+                              orientation_quat,
+                              current_time,
+                              "base_link",
+                              "odom")
 
-        self.odom_msg.twist.twist.angular.x = angular_vel_x
-        self.odom_msg.twist.twist.angular.y = angular_vel_y
-        self.odom_msg.twist.twist.angular.z = angular_vel_z
-
-        self.pub_odom.publish(self.odom_msg)
-
-        tf = TransformStamped()
-        tf.header.stamp = current_time
-        tf.header.frame_id = self.odom_msg.header.frame_id
-        tf.child_frame_id = self.odom_msg.child_frame_id
-        tf.transform.translation.x = self.odom_msg.pose.pose.position.x
-        tf.transform.translation.y = self.odom_msg.pose.pose.position.y
-        tf.transform.translation.z = self.odom_msg.pose.pose.position.z
-        tf.transform.rotation = self.odom_msg.pose.pose.orientation
-        self.tf_broadcaster.sendTransform(tf)
-    
+        self.last_time = current_time
 
 if __name__ == '__main__':
     try:
-        ImuToOdomConverter()
+        ImuOdom()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
