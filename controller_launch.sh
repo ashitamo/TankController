ef0bb52b03e6228cbecfe1d215e490e6ed031994
ashitamo <hsun91chen@gmail.com>
2024-10-07 19:39:00 +0800

    modified:   controller_launch.sh

--- controller_launch.sh
+++ controller_launch.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-#export python3=/home/hsun91chen/anaconda3/envs/py_ros/bin/python
-#export python3 = python3
-python3 heartbeam.py &
-python3 sub_stall.py &
-python3 sub_throttle.py &
-python3 sub_steer.py &
+source home/D1056411/D1056628/TankContoller/devel/setup.bash
