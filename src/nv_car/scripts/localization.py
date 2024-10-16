6608cd0c46f9be94baee486205e254b12db140a1
Bacon-Su <nick92816@gmail.com>
2024-10-16 11:50:40 +0800

    sub goal topic & calculate angle error update

--- src/nv_car/scripts/localization.py
+++ src/nv_car/scripts/localization.py
@@ -2,8 +2,10 @@
 import rospy
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import PointCloud2
+from geometry_msgs.msg import PoseStamped
 from tf.transformations import euler_from_quaternion
 import sensor_msgs.point_cloud2 as pc2
+import math
 
 class LocalizationController:
     
@@ -14,13 +16,15 @@ class LocalizationController:
         self.yaw = None
 
         self.map = None
-        self.destination = [0, 0]
+        self.goal = None
 
         rospy.init_node('localization_controller', anonymous=True)
-        #lidar odom
+        # lidar odom
         rospy.Subscriber('/lio_sam/mapping/odometry', Odometry, self.odometry_callback)
-        #map_local / global 
+        # map_local / global 
         rospy.Subscriber('/lio_sam/mapping/map_local', PointCloud2, self.pointcloud_callback)
+        # get the /gaol topic from rviz 2D nav tool
+        rospy.Subscriber('/goal', PoseStamped, self.nav_tool_callback)
         
     
     def odometry_callback(self, msg):
@@ -41,11 +45,31 @@ class LocalizationController:
         #       break
         self.map = point_cloud
 
+    def nav_tool_callback(self, msg):
+        position = msg.pose.position
+        self.goal = position # position.x position.y position.z
 
     def calculate_error(self):
         x0, y0 = self.x0, self.y0
-        x1, y1 = self.destination
+        x1, y1 = self.goal.x, self.goal.y
+
         return ((x0 - x1) ** 2 + (y0 - y1) ** 2) ** 0.5
+    
+    def calculate_angle(self):
+        x0, y0 = self.x0, self.y0
+        x1, y1 = self.goal.x, self.goal.y
+
+        v = (x1 - x0, y1 - y0)
+        print(f"Vector: {v}")
+
+        # angle = math.acos(np.dot(v, frame_x)/np.linalg.norm(v)) / math.pi * 180
+
+        # if v[1] < 0:
+        #     angle = -angle
+
+        angle = math.atan2(v[1], v[0]) / math.pi * 180
+
+        return self.yaw - angle # negative angle --> destination at left / positive --> right
     
     def publish(self):
         pass
